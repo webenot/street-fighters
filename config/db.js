8fa6ae89c646221c3a92a51373a6af2a55dea1da
webenot <webenot@mail.ua>
2019-05-24 22:14:53 +0300

    Добавлена работа с MongoDB, а также регистрация и авторизация пользователей

--- config/db.js
+++ config/db.js
@@ -1,6 +1,7 @@
 module.exports = {
     db: {
-        uri: 'mongodb://localhost:27017/heroku_zfh39rpk',
+        //uri: 'mongodb://localhost:27017/heroku_zfh39rpk',
+        uri: MONGODB_URI,
         connect: {
             config: {
                 autoIndex: false,
